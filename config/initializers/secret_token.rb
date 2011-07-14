85f3e1ec42b0a1436da4e59300fe4b92e5696542
Mathieu Davy <matt@ekynoxe.com>
2011-07-14 11:10:35 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VytaminCom::Application.config.secret_token = 'b8494e1ee887f5e593e6cdb6f400e6f062db2d7b730e8641eb65044a17820194f442b2e1550057bfc5aaca3ad8bd83462fd0427f612db9fd45c75688dcb1844c'
