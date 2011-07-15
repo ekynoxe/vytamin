1a849fdab11fb04e03120a63193544975b7f0f4f
Mathieu Davy <matt@ekynoxe.com>
2011-07-15 14:28:45 +0100

    adding entries controller and updating routes, including root to the entries index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 VytaminCom::Application.routes.draw do
 
+  resources :entries
   
   namespace :admin do |admin|
     resources :sports
   end
 
+  root :to => "entries#index"
+  match "*path", :to => redirect("/")
 end
