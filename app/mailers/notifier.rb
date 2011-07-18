bf4f86ce2bdef09d5bfa62d4a225fd41f0022367
Mathieu Davy <matt@ekynoxe.com>
2011-07-18 16:56:51 +0100

    Adding forgot password notifier

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,16 @@
+class Notifier < ActionMailer::Base
+  
+  def forgot_password(user,domain)
+    from "Vytamin Notifier <noreply@#{domain}>"
+
+    @reset_password_link = reset_password_url(user.perishable_token)
+
+    mail(:to => user.email_address_with_name,
+      :subject => "Password Reset",
+      :from => from,
+      :fail_to => from
+      ) do |format|
+        format.text
+    end
+  end
+end
